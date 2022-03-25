0f5c2c41ef2aef36879dd3c9828cfcaf2f5e80cd
Chun2Maru <57833134+Chun2Maru@users.noreply.github.com>
2022-03-25 11:47:56 +0700

    update to 0.8.1-alpha

--- buildSrc/src/main/kotlin/com/lexwilliam/dependencies/AndroidSettings.kt
+++ buildSrc/src/main/kotlin/com/lexwilliam/dependencies/AndroidSettings.kt
@@ -1,7 +1,7 @@
 package com.lexwilliam.dependencies
 
 object AndroidSettings {
-    const val appVersionName = "0.8.0-alpha"
+    const val appVersionName = "0.8.1-alpha"
     const val compileSdk = 31
     const val buildTools = "30.0.3"
     const val minSdk = 26
