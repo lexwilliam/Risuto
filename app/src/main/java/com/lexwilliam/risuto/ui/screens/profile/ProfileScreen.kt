136634bf33b841f1b8069e9905bbc0ba6f358f92
Chun2Maru <57833134+Chun2Maru@users.noreply.github.com>
2022-07-26 21:02:10 +0700

    try mpandroidchart on piechart

--- app/src/main/java/com/lexwilliam/risuto/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/lexwilliam/risuto/ui/screens/profile/ProfileScreen.kt
@@ -1,5 +1,8 @@
 package com.lexwilliam.risuto.ui.screens.profile
 
+import android.graphics.Typeface
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -10,17 +13,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 import com.google.accompanist.insets.navigationBarsWithImePadding
 import com.lexwilliam.risuto.model.UserProfilePresentation
 import com.lexwilliam.risuto.ui.component.LoadingScreen
 import com.lexwilliam.risuto.ui.component.NetworkImage
 import com.lexwilliam.risuto.ui.component.StatusBarSpacer
-import com.lexwilliam.risuto.ui.theme.RisutoTheme
+import com.lexwilliam.risuto.ui.theme.*
+import timber.log.Timber
 
 @Composable
 fun ProfileScreen(
@@ -51,6 +61,7 @@ fun ProfileContent(
             username = userProfile.username,
             joined = userProfile.joined
         )
+        ProfileStatistics(statistics = userProfile.statistics)
     }
 
 }
@@ -84,13 +95,80 @@ fun ProfileInfo(
     }
 }
 
-@Composable
-fun ProfileStatistics() {
-    AndroidView(factory = { context ->
-        PieChart(context).apply {
+data class PieChartData(
+    val name: String,
+    val count: Float
+)
 
-        }
-    })
+@Composable
+fun ProfileStatistics(
+    statistics: UserProfilePresentation.Data.Statistics
+) {
+    val pieChartData = listOf(
+        PieChartData("Completed", statistics.anime.completed.toFloat()),
+        PieChartData("Watching", statistics.anime.watching.toFloat()),
+        PieChartData("Dropped", statistics.anime.dropped.toFloat()),
+        PieChartData("On Hold", statistics.anime.on_hold.toFloat()),
+        PieChartData("Plan To Watch", statistics.anime.plan_to_watch.toFloat())
+    )
+    Column(
+        modifier = Modifier
+            .padding(18.dp)
+            .size(320.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        AndroidView(
+            factory = { context ->
+                PieChart(context).apply {
+                    layoutParams = LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    this.description.isEnabled = false
+                    this.isDrawHoleEnabled = false
+                    this.legend.isEnabled = true
+                    this.legend.textSize = 14F
+                    this.legend.horizontalAlignment =
+                        Legend.LegendHorizontalAlignment.CENTER
+                    this.setEntryLabelColor(Color.Black.hashCode())
+                }
+            },
+            update = {
+                updatePieChartWithData(it, pieChartData)
+            }
+        )
+    }
+}
+
+fun updatePieChartWithData(
+    chart: PieChart,
+    data: List<PieChartData>
+) {
+    val entries = ArrayList<PieEntry>()
+    data.forEach {
+        entries.add(PieEntry(it.count, it.name))
+    }
+
+    val ds = PieDataSet(entries, "")
+
+    ds.colors = arrayListOf(
+        primary.toArgb(),
+        primaryVariant.toArgb(),
+        secondary.toArgb(),
+        secondaryVariant.toArgb(),
+        surface.toArgb()
+    )
+
+    ds.yValuePosition = PieDataSet.ValuePosition.INSIDE_SLICE
+    ds.xValuePosition = PieDataSet.ValuePosition.INSIDE_SLICE
+    ds.sliceSpace = 2f
+    ds.valueTextColor = Color.Black.toArgb()
+    ds.valueTextSize = 18f
+    ds.valueTypeface = Typeface.DEFAULT_BOLD
+    val d = PieData(ds)
+    chart.data = d
+    chart.invalidate()
 }
 
 @Preview
