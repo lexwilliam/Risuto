6418cca42c01a7e56b0703f4850eea84096e58e3
lexwilliam <william.gumdamx@gmail.com>
2025-08-15 15:47:50 +0700

    update to api 36

--- buildSrc/src/main/kotlin/com/lexwilliam/dependencies/AndroidSettings.kt
+++ buildSrc/src/main/kotlin/com/lexwilliam/dependencies/AndroidSettings.kt
@@ -3,9 +3,9 @@ package com.lexwilliam.dependencies
 object AndroidSettings {
     const val appVersionName = "1.0.0"
     const val appVersionCode = 4
-    const val compileSdk = 35
+    const val compileSdk = 36
     const val buildTools = "30.0.3"
     const val minSdk = 26
-    const val targetSdk = 35
+    const val targetSdk = 36
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
